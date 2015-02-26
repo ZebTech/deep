6edb24524240cf492d4152b90a57b2248a55b604
Gabriel Pereyra <gbrl.pereyra@gmail.com>
2015-02-26 09:02:26 -0800

    added piano midi dataset loader

--- deep/datasets/load.py
+++ deep/datasets/load.py
@@ -16,6 +16,7 @@ import cPickle
 
 from os.path import join
 from os.path import dirname
+module_path = dirname(__file__)
 
 
 def load_mnist():
@@ -34,7 +35,6 @@ def load_mnist():
     =================   ==============
 
     """
-    module_path = dirname(__file__)
     with gzip.open(join(module_path, 'mnist', 'mnist.pkl.gz')) as data_file:
         return cPickle.load(data_file)
 
@@ -112,3 +112,29 @@ def load_plankton(test=False):
         path = '/home/gabrielpereyra/Desktop/plankton.pkl.gz'
     with gzip.open(path) as data_file:
        return cPickle.load(data_file)
+
+
+def load_paino_midi():
+    """Load and return the piano midi dataset (sequential).
+
+    :reference: http://www-etud.iro.umontreal.ca/~boulanni/icml2012
+
+    Each datapoint is a 28x28 image of a digit.
+
+    =================   ==============
+    Classes                         10
+    Samples per class             ~350
+    Samples total                33000
+    Dimensionality                 784
+    Features                floats 0-1
+    =================   ==============
+
+    """
+    with gzip.open(join(module_path, 'midi', 'piano_midi.pkl.gz')) as data_file:
+        #: repickle this in the mnist format instead
+        #: of doing it manually
+        dataset = cPickle.load(data_file)
+        train = dataset['train']
+        valid = dataset['valid']
+        test = dataset['test']
+        return train, valid, test
