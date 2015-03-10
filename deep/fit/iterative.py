1b00b20ebaa3f61be7e9f1d655ddf426ed4c4d2a
Gabriel Pereyra <gbrl.pereyra@gmail.com>
2015-03-10 15:36:33 -0700

    moved all function compilation to model

--- deep/fit/iterative.py
+++ deep/fit/iterative.py
@@ -1,6 +1,4 @@
-from time import time
 from base import Fit
-from theano import shared, config, function
 
 import theano.tensor as T
 import numpy as np
@@ -15,38 +13,13 @@ class Iterative(Fit):
         self.y = T.lvector()
         self.i = T.lscalar()
 
-    def givens(self, X, y):
-        givens = dict()
-        batch_start = self.i * self.batch_size
-        batch_end = (self.i+1) * self.batch_size
-        X = shared(np.asarray(X, dtype=config.floatX))
-        y = shared(np.asarray(y, dtype='int64'))
-        givens[self.x] = X[batch_start:batch_end]
-        givens[self.y] = y[batch_start:batch_end]
-        return givens
-
-    def compile_batch_function(self, X, y, output, updates=None):
-        output = output(self.x, self.y)
-        givens = self.givens(X, y)
-        if updates is not None:
-            updates = updates(self.x, self.y)
-        return function([self.i], output, None, updates, givens)
-
     def fit_model(self, model, X, y):
-        score = model._symbolic_score(self.x, self.y)
-        updates = model._symbolic_updates(self.x, self.y)
-        givens = self.givens(X, y)
-        train = function([self.i], score, None, updates, givens)
+        train = model.fit_function(X, y)
         batches = len(X) / self.batch_size
 
         for epoch in range(1, self.n_epochs+1):
             batch_scores = [train(batch) for batch in range(batches)]
             model.fit_scores.append(np.mean(batch_scores))
-
-        model.predict = function([self.x], model._symbolic_predict(self.x))
-        model.predict_proba = function([self.x], model._symbolic_predict_proba(self.x))
-        model.score = function([self.x, self.y], model._symbolic_score(self.x, self.y))
-
 
     def fit_validate_model(self, model, X, y, X_valid, y_valid):
         raise NotImplementedError
--- deep/models/nn.py
+++ deep/models/nn.py
@@ -1,6 +1,10 @@
+import numpy as np
 import theano.tensor as T
 _x = T.matrix()
-_y = T.vector()
+_y = T.lvector()
+_i = T.lscalar()
+
+from theano import function, shared, config
 
 from deep.costs import NegativeLogLikelihood, PredictionError
 from deep.updates import Momentum
@@ -9,10 +13,11 @@ from deep.fit import Iterative
 
 class NN(object):
 
-    def __init__(self, layers, learning_rate=0.1, update=Momentum(),
+    def __init__(self, layers, learning_rate=0.1, batch_size=128, update=Momentum(),
                  fit_type=Iterative(), cost=NegativeLogLikelihood()):
         self.layers = layers
         self.learning_rate = learning_rate
+        self.batch_size = batch_size
         self.update = update
         self.cost = cost
         self.fit_scores = []
@@ -71,7 +76,26 @@ class NN(object):
         self.fit_model(self, X, y)
         return self
 
+    def fit_givens(self, X, y):
+        givens = dict()
+        batch_start = _i * self.batch_size
+        batch_end = (_i+1) * self.batch_size
+        X = shared(np.asarray(X, dtype=config.floatX))
+        y = shared(np.asarray(y, dtype='int64'))
+        givens[_x] = X[batch_start:batch_end]
+        givens[_y] = y[batch_start:batch_end]
+        return givens
+
+    def fit_function(self, X, y):
+        score = self._symbolic_score(_x, _y)
+        updates = self._symbolic_updates(_x, _y)
+        givens = self.fit_givens(X, y)
+        return function([_i], score, None, updates, givens)
+
     def fit_validate(self, dataset):
+        raise NotImplementedError
+
+    def fit_validate_function(self, dataset):
         raise NotImplementedError
 
     def fit_layers(self, shape):
