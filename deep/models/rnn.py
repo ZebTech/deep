3ea1a28f18ecfeb21badf78395e3505ed41e3ee6
Gabriel Pereyra <gbrl.pereyra@gmail.com>
2015-03-11 22:11:46 -0700

    factored out rnn layer

--- deep/models/rnn.py
+++ deep/models/rnn.py
@@ -8,28 +8,31 @@ import matplotlib.pyplot as plt
 
 from deep.layers.base import Layer
 from deep.updates import GradientDescent
-
+from deep.initialize import Normal
 
 class RecurrentLayer(Layer):
 
-    def __init__(self, n_hidden, activation):
+    def __init__(self, n_hidden, activation, initialize=Normal()):
         self.n_hidden = n_hidden
         self.activation = activation
+        self.initialize = initialize
 
-    def __call__(self, X):
-        pass
+    def __call__(self, X, H):
+        return self.activation(T.dot(H, self.W_hidden) + T.dot(X, self.W_visible) + self.b)
 
     def fit(self, incoming_shape):
-        size = incoming_shape[1], self.n_hidden
+        size = self.n_hidden, self.n_hidden
         self.W_hidden = self.initialize.W(size)
+        size = incoming_shape[1], self.n_hidden
+        self.W_visible = self.initialize.W(size)
         self.b = self.initialize.b(self.n_hidden)
         return self
 
     @property
     def params(self):
         if self.activation.params is not None:
-            return self.W, self.b, self.activation.params
-        return self.W, self.b
+            return self.W_visible, self.W_hidden, self.b, self.activation.params
+        return self.W_visible, self.W_hidden, self.b
 
     @property
     def shape(self):
@@ -37,12 +40,6 @@ class RecurrentLayer(Layer):
 
 
 class RNN(NN):
-
-    def softmax_layer(self, n_hidden, nout, rng):
-        self.W = np.asarray(rng.normal(size=(n_hidden, nout), scale=.01, loc=0.0), dtype=theano.config.floatX)
-        self.b = np.zeros((nout,), dtype=theano.config.floatX)
-        self.W = theano.shared(self.W, 'W_hy')
-        self.b = theano.shared(self.b, 'b_hy')
 
     def recurrent_layer(self, n_hidden, nin, rng):
         self.W_uh = np.asarray(rng.normal(size=(nin, n_hidden), scale=.01, loc=.0), dtype=theano.config.floatX)
@@ -52,22 +49,27 @@ class RNN(NN):
         self.W_hh = theano.shared(self.W_hh, 'W_hh')
         self.b_hh = theano.shared(self.b_hh, 'b_hh')
         self.activ = T.nnet.sigmoid
-        self.h0_tm1 = theano.shared(np.zeros(n_hidden, dtype=theano.config.floatX))
+        self.recurrent_params = [self.W_hh, self.W_uh, self.b_hh]
 
-    def __init__(self, nin, n_hidden, nout, update=GradientDescent()):
+    def __init__(self, nin, n_hidden, output_layer, update=GradientDescent()):
         self.update = update
-
-        #: output layer
-        from deep.layers import Dense
-        from deep.activations import Identity
-        self.output_layer = Dense(nout, Identity())
-        shape = 10, n_hidden
-        self.output_layer.fit(shape)
+        self.h0_tm1 = theano.shared(np.zeros(n_hidden, dtype=theano.config.floatX))
 
         rng = np.random.RandomState(1234)
         self.lr = T.scalar()
         x = T.matrix()
         y = T.scalar()
+
+
+        self.output_layer = output_layer
+        shape = 10, n_hidden
+        self.output_layer.fit(shape)
+
+        from deep.activations import Sigmoid
+        self.recurrent_layer_ = RecurrentLayer(n_hidden, Sigmoid())
+        shape = 10, nin
+        self.recurrent_layer_.fit(shape)
+
 
         self.recurrent_layer(n_hidden, nin, rng)
 
@@ -77,14 +79,12 @@ class RNN(NN):
 
     @property
     def params(self):
-        return [self.W_hh, self.W_uh, self.b_hh] + list(self.output_layer.params)
-        #return [self.W_hh, self.W_uh, self.W, self.b_hh, self.b]
+        return list(self.recurrent_layer_.params) + list(self.output_layer.params)
+        #return self.recurrent_params + list(self.output_layer.params)
 
     def _symbolic_predict(self, x):
-        h, _ = theano.scan(self.recurrent_fn, x, [self.h0_tm1], [self.W_hh, self.W_uh, self.b_hh])
-
+        h, _ = theano.scan(self.recurrent_fn, x, [self.h0_tm1])
         return self.output_layer(h[-1])
-        #return T.dot(h[-1], self.W) + self.b
 
     def _symbolic_score(self, x, y):
         cost = ((y - self._symbolic_predict(x)) ** 2).mean(axis=0).sum()
@@ -97,9 +97,9 @@ class RNN(NN):
             updates.extend(self.update(cost, param, self.lr))
         return updates
 
-    def recurrent_fn(self, u_t, h_tm1, W_hh, W_uh, b_hh):
-        h_t = self.activ(T.dot(h_tm1, W_hh) + T.dot(u_t, W_uh) + b_hh)
-        return h_t
+    def recurrent_fn(self, u_t, h_tm1):
+        return self.recurrent_layer_(u_t, h_tm1)
+        #return self.activ(T.dot(h_tm1, self.W_hh) + T.dot(u_t, self.W_uh) + self.b_hh)
 
     def fit(self, X, y):
         vals = []
@@ -112,8 +112,10 @@ class RNN(NN):
 
 
 if __name__ == '__main__':
+    from deep.layers import Dense
+    from deep.activations import Identity
 
-    rnn = RNN(2, 50, 1)
+    rnn = RNN(2, 50, Dense(1, Identity()))
     lr = 0.0001
 
     X = np.random.rand(100, 100, 2)
