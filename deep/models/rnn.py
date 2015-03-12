befb4b083100804997e8318989123cc9e0e5ebc1
Gabriel Pereyra <gbrl.pereyra@gmail.com>
2015-03-12 10:32:14 -0700

    factored out givens

--- deep/models/rnn.py
+++ deep/models/rnn.py
@@ -1,11 +1,15 @@
 from deep.models import NN
+import theano.tensor as T
+_x = T.matrix()
+_y = T.scalar()
+_i = T.lscalar()
 
 import theano
-import theano.tensor as T
 import numpy as np
 import matplotlib.pyplot as plt
 
 from deep.updates import GradientDescent
+from theano import shared, config
 
 
 class RNN(NN):
@@ -26,6 +30,13 @@ class RNN(NN):
         recurrent_params = [param for layer in self.recurrent_layers for param in layer.params]
         return recurrent_params + output_params
 
+    def _symbolic_updates(self, x, y):
+        cost = self._symbolic_score(x, y)
+        updates = list()
+        for param in self.params:
+            updates.extend(self.update(cost, param, self.learning_rate))
+        return updates
+
     def _symbolic_recurrence(self, x, h):
         for layer in self.recurrent_layers:
             h = layer(x, h)
@@ -42,31 +53,35 @@ class RNN(NN):
     def _symbolic_score(self, x, y):
         return ((y - self._symbolic_predict(x)) ** 2).mean(axis=0).sum()
 
-    def _symbolic_updates(self, x, y):
-        cost = self._symbolic_score(x, y)
-        updates = list()
-        for param in self.params:
-            updates.extend(self.update(cost, param, self.learning_rate))
-        return updates
-
     def fit(self, X, y):
-        x = T.matrix()
-        t = T.scalar()
-
-        shape = X[0].shape
-        for layer in self.recurrent_layers + self.output_layers:
-            layer.fit(shape)
-            shape = layer.shape
-
-        cost = self._symbolic_score(x, t)
-        updates = self._symbolic_updates(x, t)
-
-        self.train_step = theano.function([x, t], cost, updates=updates)
+        self.fit_layers(X[0].shape)
+        cost = self._symbolic_score(_x, _y)
+        updates = self._symbolic_updates(_x, _y)
+        givens = self.fit_givens(X, y)
+        self.train_step = theano.function([_i], cost, updates=updates, givens=givens)
 
         vals = []
         for i in range(10):
-            for x, y_ in zip(X, y):
-                c = self.train_step(x, y_)
+            #for x, y_ in zip(X, y):
+            #    c = self.train_step(x, y_)
+            for j in range(len(X)):
+                c = self.train_step(j)
                 vals.append(c)
             plt.plot(vals)
         plt.show()
+
+    def fit_givens(self, X, y):
+        givens = dict()
+        X = shared(np.asarray(X, dtype=config.floatX))
+        y = shared(np.asarray(y, dtype='float64'))
+        givens[_x] = X[_i]
+        givens[_y] = y[_i]
+        return givens
+
+    def fit_layers(self, shape):
+        for layer in self.recurrent_layers + self.output_layers:
+            try:
+                shape = layer.shape
+            except AttributeError:
+                layer.fit(shape)
+                shape = layer.shape
